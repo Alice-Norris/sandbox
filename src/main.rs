b5e6924ef1313f5af201305b6ef52111ccc87051
Alice Kneller <alice.norris@snhu.edu>
2024-04-14 20:30:50 -0400

    tui hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use cursive::views::TextView;
+
 fn main() {
-    println!("Hello, world!");
+    let mut sis = cursive::default();
+
+    sis.add_global_callback('q', |sis| sis.quit());
+
+    sis.add_layer(TextView::new("Heckin', yeah, cool."));
+
+    sis.run();
 }
